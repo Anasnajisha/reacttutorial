9f9d6f4714b8b4c5a2366d01e2e1de5aa05a4289
NPK13 <nishalbabupk@gmail.com>
2023-06-16 10:14:34 +0530

    reacttutuorial

---
+++ src/Library.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Library() {
+  return (
+    <div>
+      <h1>this college library</h1>
+    </div>
+  )
+}
---
+++ src/Office.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Office() {
+  return (
+    <div>
+      <h1>administrative office</h1>
+    </div>
+  )
+}
